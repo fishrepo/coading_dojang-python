1cc02e8e7671f5ec122385c151398c651522bccf
seafish <seafish5252@gmail.com>
2022-12-24 02:28:00 +0900

    test

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-files = input().split()
-
-x = map(lambda x: '{0:03d}.{1}'.format(
-    int(x.split('.')[0]), x.split('.')[1]), files)
-x = list(x)
-# list(map(lambda x: '{0:03d}.{1}'.format(int(x.split('.')[0]),x.split('.')[1]) ,files))
-print(x)
+print('hello world')
