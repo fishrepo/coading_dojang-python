5d276eb58d4cb8737e6d9e74983b11512ee11be2
seafish <seafish5252@gmail.com>
2022-12-24 03:28:18 +0900

    unit33 closure

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world')
+def countdown(n):
+    stack = n+1
+
+    def count():
+        nonlocal stack
+        stack -= 1
+        return stack
+    return count
+
+
+n = int(input())
+
+c = countdown(n)
+for i in range(n):
+    print(c(), end=' ')
