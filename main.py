7f606e3e69a26931c773b29451172b7699f5c6f6
seafish <seafish5252@gmailc.om>
2022-12-24 02:22:18 +0900

    unit32 lambda

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-korean, english, mathematics, science = map(int, input().split())
+files = input().split()
 
-
-def get_min_max_score(*args):
-    return min(args), max(args)
-
-
-# 딕셔너리를 언패킹 하여 인수로 가져오면 키:값 형태이므로 for 문을 사용하여 합을 구했다.
-def get_average(**kwargs):
-
-    x = 0  # 초기화 안하면 오류생김
-    for kw, arg in kwargs.items():
-        x += arg
-
-    return x/len(kwargs)
-
-
-min_score, max_score = get_min_max_score(korean, english, mathematics, science)
-average_score = get_average(
-    korean=korean, english=english, mathematics=mathematics, science=science)
-
-print('낮은점수:{0: .2f}, 높은 점수 :{1: .2f}, 평균점수 : {2:.2f}'.format(
-    min_score, max_score, average_score))
-
-min_score, max_score = get_min_max_score(english, science)
-average_score = get_average(english=english, science=science)
-print('낮은점수:{0: .2f}, 높은 점수 :{1: .2f}, 평균점수 : {2:.2f}'.format(
-    min_score, max_score, average_score))
+x = map(lambda x: '{0:03d}.{1}'.format(
+    int(x.split('.')[0]), x.split('.')[1]), files)
+x = list(x)
+# list(map(lambda x: '{0:03d}.{1}'.format(int(x.split('.')[0]),x.split('.')[1]) ,files))
+print(x)
