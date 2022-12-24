a251bd52fdc30b69f78733f4d57aefda91863710
seafish <seafish5252@gmail.com>
2022-12-25 05:18:26 +0900

    unit 36 super,sub class

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-class Time:
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-
-    @staticmethod
-    def is_time_valid(string):
-        hour, minute, second = map(int, str(string).split(':'))
-        return hour <= 24 and minute <= 59 and second <= 60
-
-    @classmethod
-    def from_string(cls, string):
-        x, y, z = str(string).split(':')
-        x, y, z = int(x), int(y), int(z)
-
-        p = cls(x, y, z)
-
-        return p
+class Animal:
+    def eat(self):
+        print('먹다')
 
 
-time_string = input()
+class Wing:
+    def flap(self):
+        print('파닥거리다')
 
-if Time.is_time_valid(time_string):
-    t = Time.from_string(time_string)
-    print(t.hour, t.minute, t.second)
-else:
-    print('잘못된 시간 형식입니다.')
+
+class Bird(Animal, Wing):
+    def fly(self):
+        print('날다')
+
+
+b = Bird()
+b.eat()
+b.flap()
+b.fly()
+print(issubclass(Bird, Animal))
+print(issubclass(Bird, Wing))
