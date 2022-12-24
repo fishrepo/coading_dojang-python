9d24f75e1d3142023139a829e04896376751ab5a
seafish <seafish5252@gmail.com>
2022-12-25 02:38:44 +0900

    unit 35 static,class method

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-class Annie:
-    def __init__(self, health, mana, AP):
-        self.health = health
-        self.mana = mana
-        self.AP = AP
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
-    def tibbers(self):
-        damage = self.AP * 0.65 + 400
+    @staticmethod
+    def is_time_valid(string):
+        hour, minute, second = map(int, str(string).split(':'))
+        return hour <= 24 and minute <= 59 and second <= 60
 
-        print('티버: 피해량 ', damage)
+    @classmethod
+    def from_string(cls, string):
+        x, y, z = str(string).split(':')
+        x, y, z = int(x), int(y), int(z)
+
+        p = cls(x, y, z)
+
+        return p
 
 
-a = Annie(1803.68, 1184.0, 645)
+time_string = input()
 
-a.tibbers()
+if Time.is_time_valid(time_string):
+    t = Time.from_string(time_string)
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
