4c36b971b2a4c13247285ab61e699d7b51c5feab
seafish <seafish5252@gmail.com>
2022-12-27 02:20:27 +0900

    unit 39 iterator

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-class NotPalindromeError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__('회문이 아닙니다')
+class TimeIterator():
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __getitem__(self, index):  # 반복가능한 객체를 만들기 위해 인덱스 값 만큼 리턴을 반복함
+        hour = (self.start + index) // 60 // 60 % 24
+        min = (self.start + index) // 60 % 60
+        sec = (self.start + index) % 60
+        if index < self.stop - self.start:
+            return '{0:02d}:{1:02d}:{2:02d}'.format(hour, min, sec)
+        else:
+            raise IndexError
 
 
-def palindrome(word):
-    if word == word[::-1]:
-        print(word)
-    else:
-        raise NotPalindromeError
+start, stop, index = map(int, input().split())
 
+for i in TimeIterator(start, stop):
+    print(i)
 
-try:
-    word = input()
-    palindrome(word)
-except NotPalindromeError as e:
-    print(e)
+print('\n', TimeIterator(start, stop)[index], sep='')
