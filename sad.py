9122ff1b2648b503bbe4042a7319a028f8965848
seafish <seafish5252@gmail.com>
2022-12-28 19:13:19 +0900

    unit42 decorator -2

---
+++ sad.py
@@ -0,0 +1,15 @@
+def is_multiple(func):
+    def wrapper(a, b):
+        r = func(a, b)
+        x = 3
+        if r % x == 0:
+            print('{0}의 반환값은 {1}의 배수 입니다.'.format(func.__name__, x))
+        else:
+            print('{0}의 반환값은 {1}의 배수가 아닙니다.'.format(func.__name__, x))
+        return r
+    return wrapper
+
+
+@is_multiple
+def add(a, b):
+    return a+b
